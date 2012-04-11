47e6249d83b45a57903a8567cea828d57365724f
firefly-cpp <pingvin1@gmail.com>
2012-04-11 23:05:38 +0300

    Committing version 1.0.

--- Firefly.cpp
+++ Firefly.cpp
@@ -9,7 +9,9 @@
 
 /* Reference Paper*/
 
-/*I. Fister Jr.,  X.-S. Yang,  I. Fister, J. Brest, Memetic firefly algorithm for combinatorial optimization, in Bioinspired Optimization Methods and their Applications (BIOMA 2012), B. Filipic and J.Silc, Eds. Jozef Stefan Institute, Ljubljana, Slovenia, 2012 */
+/*I. Fister Jr.,  X.-S. Yang,  I. Fister, J. Brest, Memetic firefly algorithm for combinatorial optimization, 
+in Bioinspired Optimization Methods and their Applications (BIOMA 2012), B. Filipic and J.Silc, Eds. 
+Jozef Stefan Institute, Ljubljana, Slovenia, 2012 */
 
 /*Contact:
 Iztok Fister (iztok.fister@uni-mb.si)
